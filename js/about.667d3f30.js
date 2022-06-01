90e03a2b9b70a5f7ae35d7c4c476c96ecf3434c4
Sungsil <sungsil.universe@gmail.com>
2022-06-01 13:18:06 +0900

    Add files via upload

---
+++ js/about.667d3f30.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkhello"]=self["webpackChunkhello"]||[]).push([[443],{7381:function(n,e,u){u.r(e),u.d(e,{default:function(){return h}});var t=u(3396);const a={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),s=[r];function l(n,e){return(0,t.wg)(),(0,t.iD)("div",a,s)}var c=u(89);const o={},i=(0,c.Z)(o,[["render",l]]);var h=i}}]);
+//# sourceMappingURL=about.667d3f30.js.map
